a5bc3234e919a7c454dbae0215540d6b12125800
Aly <100826322+Visontt@users.noreply.github.com>
2023-05-12 01:49:59 +0700

    Add files via upload

---
+++ Models/Characters/characterStats.cs
@@ -0,0 +1,33 @@
+﻿// using UnityEngine;
+
+public class CharacterStats
+{
+    private int health = 100;
+    private int strength = 10;
+    
+    public int Health 
+    {
+        get { return health; }
+        // set { health = Mathf.Max(0, value); }
+    }
+    
+    public int Strength 
+    {
+        get { return strength; }
+        // set { strength = Mathf.Max(0, value); }
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        // Health -= damage;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+    
+    private void Die()
+    {
+        // Code to handle character death
+    }
+}
